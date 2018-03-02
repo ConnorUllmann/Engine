543bf40c47fba1abfb8b85f1b05aac2a3e54df16
ConnorUllmann <c.ullmann@hotmail.com>
2018-03-01 23:12:06 -0500

    Move additional polygon-based classes into their own files

---
+++ Engine/PolygonActor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using System.Linq;
+
+namespace Engine
+{
+    public class PolygonActor : Actor
+    {
+        public Polygon Polygon;
+
+        public PolygonFillRenderer FillRenderer;
+        public PolygonOutlineRenderer OutlineRenderer;
+
+        public bool FillVisible = true;
+        public bool OutlineVisible = true;
+
+        public PolygonActor(Polygon _polygon, float _x = 0, float _y = 0, bool _center = true) : base(_x, _y)
+        {
+            Polygon = _polygon;
+            if (_center) Center();
+            FillRenderer = new PolygonFillRenderer(Polygon, X, Y, ColorExtensions.RandomColor());
+            OutlineRenderer = new PolygonOutlineRenderer(Polygon, X, Y, ColorExtensions.RandomColor());
+        }
+
+        private void UpdateBounds() => BoundingBox.SetDimensions(Polygon.BoundingRectangle());
+
+        private void Center()
+        {
+            var com = Polygon.CenterOfMass;
+            X += com.X;
+            Y += com.Y;
+            Polygon.Move(-com);
+            UpdateBounds();
+        }
+
+        public void Rotate(float _angleRad, Vector3? _center = null)
+        {
+            var centerRelative = _center.HasValue
+                ? _center.Value - new Vector3(X, Y, 0)
+                : Vector3.Zero;
+            var centerAbsolute = _center.HasValue
+                ? _center.Value
+                : Vector3.Zero;
+            Polygon.Rotate(_angleRad, centerRelative);
+            FillRenderer.Rotate(_angleRad, centerAbsolute);
+            OutlineRenderer.Rotate(_angleRad, centerAbsolute);
+            UpdateBounds();
+        }
+
+        public IEnumerable<Polygon> SplitAlongLine(Vector3 pointA, Vector3 pointB)
+        {
+            var v = new Vector3(X, Y, 0);
+            var ret = Polygon.SplitAlongLine(pointA - v, pointB - v).ToList();
+            ret.ForEach(o => o.Move(v));
+            return ret;
+        }
+
+        public override void Render()
+        {
+            if (FillVisible)
+                FillRenderer.Render(X, Y);
+            if (OutlineVisible)
+                OutlineRenderer.Render(X, Y);
+        }
+    }
+}
---
+++ Engine/PolygonRenderer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace Engine
+{
+    public abstract class PolygonRenderer
+    {
+        private ColoredVertexBuffer buffer;
+        private ColoredVertexArray array;
+
+        public bool Visible { get; set; } = true;
+
+        public PolygonRenderer(ColoredVertexBuffer _buffer)
+        {
+            buffer = _buffer;
+            array = ColoredVertexArray.FromBuffer(buffer);
+        }
+
+        public void SetColor(Color4 _color) => buffer.SetColor(_color);
+        public void RandomizeColor() => buffer.RandomizeColor();
+
+        public void Rotate(float _angleRad, Vector3 _center) => array?.Rotate(_angleRad, _center);
+        public void Move(float _x, float _y) => Move(new Vector3(_x, _y, 0));
+        public void Move(Vector3 _position) => array?.Move(_position);
+
+        public void Render(float _x, float _y) => Render(new Vector3(_x, _y, 0));
+        public void Render(Vector3 _position)
+        {
+            if (Visible)
+                array?.Render(_position);
+        }
+    }
+
+    public class PolygonOutlineRenderer : PolygonRenderer
+    {
+        public PolygonOutlineRenderer(Polygon _polygon, float _x = 0, float _y = 0, Color4? _outlineColor = null) : base(_polygon.GetOutlineBuffer(_outlineColor ?? Color4.White))
+        {
+            Move(_x, _y);
+        }
+    }
+
+    public class PolygonFillRenderer : PolygonRenderer
+    {
+        public PolygonFillRenderer(Polygon _polygon, float _x = 0, float _y = 0, Color4? _fillColor = null) : base(_polygon.GetFillBuffer(_fillColor ?? Color4.White))
+        {
+            Move(_x, _y);
+        }
+    }
+}
