11305be878162e9b553e8753333991413d2f7007
ConnorUllmann <c.ullmann@hotmail.com>
2018-03-03 20:23:03 -0500

    Add Texture2D and test image for initial texture drawing

---
+++ Engine/OpenGL/Shaders/Texture2D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.OpenGL.Shaders
+{
+    struct Texture2D
+    {
+        private int id;
+        private int width;
+        private int height;
+
+        public int ID => id;
+        public int Width => width;
+        public int Height => height;
+
+        // BMP, GIF, EXIF, JPG, PNG, TIFF
+        public Texture2D(int _id, int _width, int _height)
+        {
+            id = _id;
+            width = _width;
+            height = _height;
+        }
+    }
+}
