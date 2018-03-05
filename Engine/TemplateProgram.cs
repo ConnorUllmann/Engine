0d498078e5f9c5325f868b7d20c798d065cbae02
ConnorUllmann <c.ullmann@hotmail.com>
2018-03-04 19:28:15 -0500

    Remove Image project until System.Drawing support is available and move Ant project to its own repo

--- Engine/TemplateProgram.cs
+++ Engine/TemplateProgram.cs
@@ -4,7 +4,7 @@ using OpenTK.Graphics;
 
 namespace Engine
 {
-    public class Program
+    public class TemplateProgram
     {
         public static void Main(string[] args) => new Game(1280, 720);
     }
