52a74a4195b8289d2a8ba75ad9f1839305196b71
ConnorUllmann <c.ullmann@hotmail.com>
2018-03-15 01:16:31 -0400

    Match private variable casing in rest of library and add documentation for arguments for constructor

--- Engine/Timer.cs
+++ Engine/Timer.cs
@@ -6,36 +6,42 @@ namespace Engine
 {
     public class Timer
     {
-        private float Period;
-        private Action Action;
-        private bool Repeat;
-        private float Current;
+        private float period;
+        private Action action;
+        private bool repeat;
+        private float current;
 
+        /// <summary>
+        /// Simple class to use in combination with an update loop to time events
+        /// </summary>
+        /// <param name="_period">seconds between calls to _action</param>
+        /// <param name="_action">function to call when each _period of time has elapsed</param>
+        /// <param name="_repeat">whether to continue counting after _action is called for the first time</param>
         public Timer(float _period, Action _action = null, bool _repeat = true)
         {
-            Period = _period;
-            Action = _action;
-            Repeat = _repeat;
+            period = _period;
+            action = _action;
+            repeat = _repeat;
             Restart();
         }
 
-        public void Restart() => Current = 0;
+        public void Restart() => current = 0;
 
-        public float NormalizedPercent => Current / Period;
+        public float NormalizedPercent => current / period;
 
         public void Update()
         {
-            if (!Repeat && Current >= Period)
+            if (!repeat && current >= period)
                 return;
 
-            Current += Game.Delta;
-            if(Current >= Period)
+            current += Game.Delta;
+            if(current >= period)
             {
-                if (Repeat)
-                    Current = Basics.Utils.Clamp(Current - Period, 0, Period);
+                if (repeat)
+                    current = Basics.Utils.Clamp(current - period, 0, period);
                 else
-                    Current = Period;
-                Action?.Invoke();
+                    current = period;
+                action?.Invoke();
             }
         }
     }
